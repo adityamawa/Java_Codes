966e8e564f4e446aec9df9a4790f4ae85a15a7f1
Aditya Mawa <aditya.05mawa@gmail.com>
2024-09-16 12:59:20 +0530

    basic linear search file

---
+++ linearSearch.java
@@ -0,0 +1,36 @@
+public class linearSearch {
+
+   public static int lsearch(int arr[],int target){
+
+    for(int i = 0; i <arr.length;i++){
+        if(arr[i] == target){
+            return i;
+        }
+    }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        
+        int arr[] ={4,2,7,8,1,2,5};
+        int target = 5;
+       
+
+
+
+        int ans = lsearch(arr,target);
+        if(ans == -1){
+            System.out.println("target not found !!");
+        }else{
+            System.out.println("index of target : "+ans);
+
+        }
+
+
+
+
+    }
+
+   
+   
+}
